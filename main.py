445092f23d8241e89d1a855b2e983b0ce4e86de5
Nicolas Orjuela <n.orjuela@uniandes.edu.co>
2022-04-25 20:56:11 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from typing import Optional
+import pandas as pd
+from fastapi import FastAPI
+from joblib import load
+import DataModel
+import PredictionModel
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+   return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+   return {"item_id": item_id, "q": q}
+
+
+@app.post("/predict")
+def make_predictions(dataModel: DataModel.DataModel):
+    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    df.columns = dataModel.columns()
+    df = df.drop("Life expectancy", axis=1)
+    model = load("assets/modelo.joblib")
+    result = model.predict(df)
+    return result
+
+
+@app.post("/getrsquared")
+def get_r2(dataModel: DataModel.DataModel):
+    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    df.columns = dataModel.columns()
+    x = df.drop("Life expectancy", axis=1)
+    y = df["Life expectancy"]
+    model = load("assets/modelo.joblib")
+    result = model.score(x, y)
+    return result
+
